309836bb0c131ab06c85551845a650a9494e00df
Jay Patel <142209052+jayyypatel@users.noreply.github.com>
2023-08-14 13:50:50 +0530

    Update settings.py

--- social/settings.py
+++ social/settings.py
@@ -161,8 +161,8 @@ CHANNEL_LAYERS = {
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com' 
 EMAIL_PORT = '587' 
-EMAIL_HOST_USER = 'protubeweb156@gmail.com'
-EMAIL_HOST_PASSWORD = 'hufiklklbbuniiqb'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 #for protubeweb156@gmail.com
-#hufiklklbbuniiqb
+
 EMAIL_USE_TLS = True
